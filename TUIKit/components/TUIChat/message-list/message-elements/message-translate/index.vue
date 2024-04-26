0e4a42fc778d06a0eea56c25f82b7da061fbbf02
aallenguo <aallenguo@tencent.com>
2024-04-26 15:31:44 +0800

    release: v2.1.1

--- TUIKit/components/TUIChat/message-list/message-elements/message-translate/index.vue
+++ TUIKit/components/TUIChat/message-list/message-elements/message-translate/index.vue
@@ -1,36 +1,20 @@
 <template>
   <div
     v-if="translationVisible"
+    ref="translationWrapperRef"
     :class="{
       'message-translation': true,
       'reverse': props.message.flow === 'out',
-      'error': translationError,
+      'error': hasTranslationError,
     }"
   >
-    <div
-      class="message-translation-container"
-    >
-      <div
-        v-if="translationFinished"
-        :id="`translation-content-${props.message.ID}`"
-        ref="translationContentRef"
-        :class="{
-          'translation-content': true,
-          'occur': true
-        }"
-      >
-        {{ translationText }}
-      </div>
-      <div
-        ref="translationLoadingRef"
-        :class="{
-          'loading': true,
-          'loading-end': translationFinished
-        }"
-      >
-      {{ TUITranslateService.t('TUIChat.翻译中') }}...
-      </div>
-    </div>
+    <TranslationContent
+      :message="props.message"
+      :translationContentVisible="translationVisible"
+      :translationWrapperRef="translationWrapperRef"
+      :isSingleTranslation="isSingleTranslation"
+      @toggleErrorStatus="toggleErrorStatus"
+    />
     <div class="copyright">
       <Icon
         :file="checkIcon"
@@ -44,7 +28,7 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, watch, onMounted, onUnmounted } from '../../../../../adapter-vue';
+import { ref, onMounted, onUnmounted } from '../../../../../adapter-vue';
 import {
   TUIStore,
   StoreName,
@@ -52,29 +36,24 @@ import {
   TUITranslateService,
 } from '@tencentcloud/chat-uikit-engine';
 import Icon from '../../../../common/Icon.vue';
+import TranslationContent from './translation-content.vue';
 import checkIcon from '../../../../../assets/icon/check-sm.svg';
 import { ITranslateInfo } from '../../../../../interface';
-import { Translator } from '../../../utils/translation';
 
 interface IProps {
-  visible: boolean;
   message: IMessageModel;
 }
 
 const props = withDefaults(defineProps<IProps>(), {
-  visible: false,
   message: () => ({} as IMessageModel),
 });
 
 const translationVisible = ref<boolean>(false);
-const translationFinished = ref<boolean>(false);
-const translationError = ref<boolean>(false);
-const translationText = ref<string>('');
-const calculateHeight = ref<number>(0);
-const calculateWidth = ref<number>(0);
+const hasTranslationError = ref<boolean>(false);
 
-const translationLoadingRef = ref<HTMLDivElement>();
-const translationContentRef = ref<HTMLDivElement>();
+const translationWrapperRef = ref<HTMLDivElement>();
+
+let isSingleTranslation = true;
 
 onMounted(() => {
   TUIStore.watch(StoreName.CHAT, {
@@ -88,38 +67,26 @@ onUnmounted(() => {
   });
 });
 
+function toggleErrorStatus(hasError: boolean) {
+  hasTranslationError.value = hasError;
+}
+
 function onMessageTranslationUpdated(info: Map<string, ITranslateInfo[]>) {
   if (info === undefined) return;
+  isSingleTranslation = false;
   const translationInfoList = info.get(props.message.conversationID) || [];
   for (let i = 0; i < translationInfoList.length; ++i) {
     const { messageID, visible } = translationInfoList[i];
     if (messageID === props.message.ID && visible !== undefined) {
+      if (translationInfoList.length === 1 && visible) {
+        isSingleTranslation = true;
+      }
+      hasTranslationError.value = false;
       translationVisible.value = visible;
       break;
     }
   }
 }
-
-watch(translationVisible, (newVal: boolean) => {
-  if (newVal) {
-    Translator.get(props.message)
-      .then((text) => {
-        translationFinished.value = true;
-        translationText.value = text;
-      })
-      .catch((err) => {
-        translationFinished.value = true;
-        translationError.value = true;
-        translationText.value = err.message;
-      });
-  } else {
-    translationFinished.value = false;
-    translationError.value = false;
-    translationText.value = '';
-    calculateHeight.value = 0;
-    calculateWidth.value = 0;
-  }
-});
 </script>
 
 <style lang="scss" scoped>
@@ -135,40 +102,6 @@ watch(translationVisible, (newVal: boolean) => {
 
   &.error {
     background-color: #ffdfdf;
-  }
-
-  .message-translation-container {
-    min-height: 16px;
-    min-width: 80px;
-    position: relative;
-    transition: width 0.15s ease-out, height 0.15s ease-out, ;
-    font-size: 14px;
-
-    .loading {
-      position: absolute;
-      top: 0;
-      left: 0;
-      opacity: 1;
-      transition: opacity 0.3s ease-out;
-
-      &.loading-end {
-        opacity: 0;
-      }
-    }
-
-    .translation-content {
-      opacity: 0;
-
-      &.occur {
-        animation: occur 0.3s ease-out 0.35s forwards;
-
-        @keyframes occur {
-          100% {
-            opacity: 1;
-          }
-        }
-      }
-    }
   }
 
   .copyright {
