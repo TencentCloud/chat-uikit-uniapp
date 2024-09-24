81b19cc4b6254360e38bbca9e1d65e66993b4b23
aallenguo <aallenguo@tencent.com>
2024-09-24 16:06:49 +0800

    release: v2.2.8

---
+++ sample/locales/zh_tw/index.ts
@@ -0,0 +1,15 @@
+import Login from './login.json';
+
+const messages = {
+  zh_tw: {
+    当前语言: '繁體中文',
+    即时通信: '即時通信',
+    即时通信IM: '即時通信IM',
+    社交娱乐: '社交娛樂',
+    腾讯云: '騰訊雲',
+    使用指引: '使用指引',
+    Login,
+  },
+};
+
+export default messages;
